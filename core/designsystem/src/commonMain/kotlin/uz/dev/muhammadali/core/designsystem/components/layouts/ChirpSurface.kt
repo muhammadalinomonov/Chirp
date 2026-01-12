0b9016d701bab18a2b44a97d60323ba3919d5b8f
muhammadali <muhammadalinomonov837@gmail.com>
2026-01-12 18:07:34 +0500

    create core surface

---
+++ core/designsystem/src/commonMain/kotlin/uz/dev/muhammadali/core/designsystem/components/layouts/ChirpSurface.kt
@@ -0,0 +1,82 @@
+package uz.dev.muhammadali.core.designsystem.components.layouts
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import chrip.core.designsystem.generated.resources.Res
+import chrip.core.designsystem.generated.resources.logo_chirp
+import org.jetbrains.compose.resources.vectorResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import uz.dev.muhammadali.core.designsystem.theme.AppTheme
+
+@Composable
+fun ChirpSurface(
+    modifier: Modifier = Modifier,
+    header: @Composable ColumnScope.() -> Unit = {},
+    content: @Composable ColumnScope.() -> Unit,
+) {
+    Surface(
+        color = MaterialTheme.colorScheme.background,
+        modifier = modifier
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier.fillMaxSize(),
+        ) {
+            header()
+            Surface(
+                color = MaterialTheme.colorScheme.surface,
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    content()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun ChirpSurfacePreview() {
+    AppTheme {
+        ChirpSurface(
+            modifier = Modifier.fillMaxSize(),
+            header = {
+                Icon(
+                    imageVector = vectorResource(Res.drawable.logo_chirp),
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier
+                        .padding(vertical = 32.dp)
+                )
+            },
+            content = {
+                Text(
+                    text = "Welcome to Chirp!",
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier
+                        .padding(vertical = 40.dp)
+                        .align(Alignment.CenterHorizontally)
+                )
+            }
+        )
+    }
+}
